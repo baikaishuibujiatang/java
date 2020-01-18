d54eb769ede3e89776e102a9a5116991df6932d3
baikaishuibujiatang <2754316320@qq.com>
2020-01-18 16:30:04 +0800

    LeetCode-9回文数

---
+++ Solution4.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+	public boolean isPalindrome2(int x) {
+        String s1=Integer.toString(x);
+        String s2=reverse(s1);
+        return s1.equals(s2);
+    }
+    public String reverse(String s){
+        int length=s.length();
+        String r="";
+        for(int i=length-1;i>=0;i--){
+            char ch=s.charAt(i);
+            r=r+ch;
+        }
+        return r;
+    }
+}
